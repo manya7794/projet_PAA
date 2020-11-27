8c06992a484e8342dec61aff5f0bd194ba1eb738
Trystan D <72400479+manya7794@users.noreply.github.com>
2020-11-27 13:35:02 +0100

    Ajout du VilleParser

---
+++ Projet_PAA/src/VilleParser.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+public class VilleParser {
+	public static Ville parser(String fichier) {
+		String nom=null;
+		boolean ecole=false;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(fichier))){
+			String ligne =null;
+			while ((ligne=br.readLine())!=null) {
+				if(ligne.startsWith("ville")){
+					if(nom!=null) {
+						System.err.println("Une ville ne peut pas avoir deux noms");
+						System.exit(0);
+					}
+					else {
+						nom= ligne.split("(")[1];
+						nom.substring(0, nom.length()-2);
+					}
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace() ;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (nom ==null) {
+			System.err.println("Le nom de la ville n'a pas été indique");
+			System.exit(0);
+		}
+		return new Ville(nom, ecole);
+	}
+}
