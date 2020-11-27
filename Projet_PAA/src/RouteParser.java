b5ab22f05b4e9e5be194de394a2a6db8de96a041
Trystan D <72400479+manya7794@users.noreply.github.com>
2020-11-27 19:01:30 +0100

    Add files via upload

--- Projet_PAA/src/RouteParser.java
+++ Projet_PAA/src/RouteParser.java
@@ -4,6 +4,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class RouteParser {
+	private static boolean sortie = false;
 	public static String parser(String fichier, int nbLignes) {
 		String villesaCouper=null;
 		
@@ -15,7 +16,7 @@ public class RouteParser {
 				while ((ligne=br.readLine())!=null) {
 					if(ligne.startsWith("route")){
 						if(villesaCouper!=null) {
-
+						
 						}
 						else {
 							villesaCouper =ligne.substring(6, ligne.length()-2);
@@ -28,10 +29,16 @@ public class RouteParser {
 			e.printStackTrace();
 		}
 		if(villesaCouper==null) {
-			System.err.println("Aucune route n'a ete indique");
+			//System.err.println("Aucune route n'a ete indique");
+			setSortie();
 			System.exit(0);
 		}
 		return villesaCouper;
 	}
-	
+	private static void setSortie() {
+		sortie =true;
+	}
+	public boolean getSortie() {
+		return sortie;
+	}
 }
--- Projet_PAA/src/VilleParser.java
+++ Projet_PAA/src/VilleParser.java
@@ -2,38 +2,41 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-
 public class VilleParser {
-	
+	private static boolean sortie=false;
 	public static Ville parser(String fichier, int nbLigne) {
 		String nom=null;
 		boolean ecole=false;
-		
 		try(BufferedReader br=new BufferedReader(
 			new FileReader(fichier))){
-			String ligne=null;
-			for(int i=0; i<nbLigne; i++) {
-				ligne=br.readLine();
-			}
-			while((ligne=br.readLine())!=null){
-				if(ligne.startsWith("ville")){
-					if(nom!=null){
-					
-					}
-					else{
-						nom=ligne.substring(6, (ligne.length()-2));
-					}
-				}
-			}
-		}catch(FileNotFoundException e){
-			e.printStackTrace();
-		}catch(IOException e){
-			e.printStackTrace();
+	String ligne=null;
+	for(int i=0; i<nbLigne; i++) {
+		ligne=br.readLine();
+	}
+	while((ligne=br.readLine())!=null){
+		if(ligne.startsWith("ville")){
+		if(nom!=null){
+			
+		}else{
+		nom=ligne.substring(6, (ligne.length()-2));
 		}
-		if(nom==null){
-			System.err.println("Le nom nâ€™a pas ete indique");
-			System.exit(0);
 		}
+			}}catch(FileNotFoundException e){
+				e.printStackTrace();
+			}catch(IOException e){
+			e.printStackTrace();
+			}
+			if(nom==null){
+			//System.err.println("Le nom n’a pas ete indique");
+			setSortie();
+			}
 		return new Ville(nom, ecole);
+	}
+	
+	private static void setSortie() {
+		sortie=true;
+	}
+	public static boolean getSortie() {
+		return sortie;
 	}
 }
