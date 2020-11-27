80695bbcd490b7c37a833e13129f7ca306dba5a8
Trystan D <72400479+manya7794@users.noreply.github.com>
2020-11-27 14:15:44 +0100

    Test du saut de lignes dans le Parser

--- Projet_PAA/src/VilleParser.java
+++ Projet_PAA/src/VilleParser.java
@@ -3,24 +3,27 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 public class VilleParser {
-	public static Ville parser(String fichier) {
+	public static Ville parser(String fichier, int nbVille) {
 		String nom=null;
 		boolean ecole=false;
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(fichier))){
 			String ligne =null;
-			while ((ligne=br.readLine())!=null) {
-				if(ligne.startsWith("ville")){
-					if(nom!=null) {
-						System.err.println("Une ville ne peut pas avoir deux noms");
-						System.exit(0);
-					}
-					else {
-						nom= ligne.split("(")[1];
-						nom.substring(0, nom.length()-2);
+			for (int i=0; i<nbVille; i++) {
+				while ((ligne=br.readLine())!=null) {
+					if(ligne.startsWith("ville")){
+						if(nom!=null) {
+							System.err.println("Une ville ne peut pas avoir deux noms");
+							System.exit(0);
+						}
+						else {
+							nom= ligne.split("(")[1];
+							nom.substring(0, nom.length()-2);
+						}
 					}
 				}
 			}
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace() ;
 		} catch (IOException e) {
