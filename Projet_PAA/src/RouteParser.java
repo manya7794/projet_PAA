329a9bc27f90dfa042d548db132960ecc4ef6e96
Trystan D <72400479+manya7794@users.noreply.github.com>
2020-11-27 18:00:59 +0100

    Ajout du RouteParser //+ simple que l'autre salope

---
+++ Projet_PAA/src/RouteParser.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class RouteParser {
+	public static String parser(String fichier, int nbLignes) {
+		String villesaCouper=null;
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(fichier))){
+			String ligne=null;
+			for(int i=0; i<nbLignes;i++) {
+				ligne=br.readLine();
+			}
+				while ((ligne=br.readLine())!=null) {
+					if(ligne.startsWith("route")){
+						if(villesaCouper!=null) {
+
+						}
+						else {
+							villesaCouper =ligne.substring(6, ligne.length()-2);
+						}
+					}
+				}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(villesaCouper==null) {
+			System.err.println("Aucune route n'a ete indique");
+			System.exit(0);
+		}
+		return villesaCouper;
+	}
+	
+}
