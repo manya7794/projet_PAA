ec9886246b4274118d9e560e88fe38e6587edf59
ClarkeZ <56111201+ClarkeZ@users.noreply.github.com>
2020-11-27 17:16:30 +0100

    Update VilleParser.java
    Incrémenter

--- Projet_PAA/src/VilleParser.java
+++ Projet_PAA/src/VilleParser.java
@@ -2,35 +2,38 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+
 public class VilleParser {
+	
 	public static Ville parser(String fichier, int nbLigne) {
 		String nom=null;
 		boolean ecole=false;
 		
 		try(BufferedReader br=new BufferedReader(
 			new FileReader(fichier))){
-	String ligne=null;
-	for(int i=0; i<nbLigne; i++) {
-		ligne=br.readLine();
-	}
-	while((ligne=br.readLine())!=null){
-		if(ligne.startsWith("ville")){
-		if(nom!=null){
-
-		}else{
-			//nom=ligne.split(":")[1];
-		nom=ligne.substring(6, (ligne.length()-2));
-		}
-		}
-			}}catch(FileNotFoundException e){
-				e.printStackTrace();
-			}catch(IOException e){
+			String ligne=null;
+			for(int i=0; i<nbLigne; i++) {
+				ligne=br.readLine();
+			}
+			while((ligne=br.readLine())!=null){
+				if(ligne.startsWith("ville")){
+					if(nom!=null){
+					
+					}
+					else{
+						nom=ligne.substring(6, (ligne.length()-2));
+					}
+				}
+			}
+		}catch(FileNotFoundException e){
 			e.printStackTrace();
-			}
-			if(nom==null){
-			System.err.println("Le nom n’a pas ete indique");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		if(nom==null){
+			System.err.println("Le nom nâ€™a pas ete indique");
 			System.exit(0);
-			}
+		}
 		return new Ville(nom, ecole);
 	}
 }
